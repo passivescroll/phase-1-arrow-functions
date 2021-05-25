457cef4c09f5803ecf6f1f58704f76d374b9b6a2
sky <flahertysky12@gmail.com>
2021-05-25 15:40:49 -0400

    Completed assignmet

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const divide = (parameter1, parameter2) => {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return parameter1 / parameter2;
+}
+sum(2000, 100);
+
